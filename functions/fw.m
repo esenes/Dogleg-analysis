276d6c73879d4381ebd551bd622ade3836210636
esenes <esenes@users.noreply.github.com>
2016-05-10 16:34:19 +0200

    Add files via upload

---
+++ functions/fw.m
@@ -0,0 +1,17 @@
+function [MEAN, PEAK,x1,x2,y0] = fw(x,y,level)
+%
+% 
+% 
+% Last modified 03.05.2016 by Theodoros Argyropoulos
+
+delta_t_pts = x(2)-x(1);
+taux = find(y>level*max(y),1);
+x1 = x(taux) - (y(taux)-level*max(y))/(y(taux)-y(taux-1)) * delta_t_pts;
+taux = find(y>level*max(y),1,'last');
+x2 = x(taux) + (y(taux)-level*max(y))/(y(taux)-y(taux+1)) * delta_t_pts;
+
+MEAN = (x1+x2)/2;               
+PEAK = max(y);
+y0 = level*max(y);
+
+end

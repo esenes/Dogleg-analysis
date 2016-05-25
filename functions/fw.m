0c11e255f73f95f9b28c63cab1dbd96829f90afd
esenes <esenes@users.noreply.github.com>
2016-05-25 14:12:15 +0200

    Update fw.m

--- functions/fw.m
+++ functions/fw.m
@@ -1,8 +1,19 @@
 function [MEAN, PEAK,x1,x2,y0] = fw(x,y,level)
-% findwidth
+%   fw.m: finds the width of the pulse at a certain level.
+%   The points selected are the lefter and the righter at
+%   the defined level. What is in the middle is not checked.
+%
+%   Inputs:
+%       - x,y
+%       - level: 
+%
+%   Outputs:
+%       - mean: 
+%       - peak:
+%       - x1,x2:
+%       - y0:
 % 
-% 
-% Last modified 03.05.2016 by Theodoros Argyropoulos
+%   Last modified 03.05.2016 by Theodoros Argyropoulos
 
 delta_t_pts = x(2)-x(1);
 taux = find(y>level*max(y),1);

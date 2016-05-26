6b1dbce1147aa15bb0324a7d06bb6305472c3b4e
esenes <esenes@users.noreply.github.com>
2016-05-26 17:53:19 +0200

    Add files via upload

--- functions/getDeltaPower.m
+++ functions/getDeltaPower.m
@@ -1,11 +1,14 @@
-function [ deltay ] = getDeltaPower( slope, x1, x2 )
+function [ deltay ] = getDeltaPower( slope, x1, x2, varargin )
 %   getDeltaPower.m returns the difference in height between the two points
 %   of height y1 and y2, using a straight line of slope m
-%
+%   if varargin is 'abs' then give back the absolute value of the diference
 %   !!!! x1 and x2 are in bins !
 
 deltay = slope*(x2-x1);
-deltay = abs(deltay);
 
+if strcmpi(varargin, 'abs')
+deltay = abs(deltay);
+end
+    
 end
 

39b8b105d473fc1750cd60561b059cb7d14fd5a6
esenes <esenes@users.noreply.github.com>
2016-06-10 14:22:33 +0200

    Add files via upload

---
+++ Dates_list.m
@@ -0,0 +1,56 @@
+%% experiments dates
+
+%% 43.3MW
+%run1
+startDate = '20160304';
+endDate = '20160307';
+startTime = '19:30:00';
+endTime = '08:00:00';
+
+%run2
+startDate = '20160324';
+endDate = '20160330';
+startTime = '18:30:00';
+endTime = '16:00:00';
+
+%run3
+startDate = '20160331';
+endDate = '20160404';
+startTime = '18:00:00';
+endTime = '02:00:00';
+
+%run4
+startDate = '20160415';
+endDate = '20160416';
+startTime = '18:45:00';
+endTime = '17:00:00';
+
+%run5
+startDate = '20160422';
+endDate = '20160425';
+startTime = '18:00:00';
+endTime = '10:00:00';
+
+%run7
+startDate = '20160504';
+endDate = '20160509';
+startTime = '18:20:00';
+endTime = '09:30:00';
+
+%run8
+startDate = '20160513';
+endDate = '20160513';
+startTime = '18:30:00';
+endTime = '22:00:00';
+
+%run9
+startDate = '20160520';
+endDate = '20160523';
+startTime = '18:00:00';
+endTime = '00:20:00';
+
+%run10
+startDate = '20160527';
+endDate = '20160530';
+startTime = '17:30:00';
+endTime = '09:00:00';

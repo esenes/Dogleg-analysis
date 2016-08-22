2b084418d5eef347e18d67a493b715c7213b3226
Eugenio Senes <esenes@users.noreply.github.com>
2016-08-22 11:43:19 +0200

    Add files via upload

--- functions/correlationMethod.m
+++ functions/correlationMethod.m
@@ -1,4 +1,22 @@
 function [coeff,gof,err] = correlationMethod( x1,y1,x2,y2,wind,tsignal)
+%   correlationMethod.m finds the correlation of the peaks in the tails
+%   of INC and REF. 
+%
+%   Inputs:
+%       - x1, y1, x2, y2: x and y arrays containing timescale and signal
+%       - wind: the length window. e.g 1:100 for 100 values
+%       - tsignal: the start bin in ns
+%
+%   Outputs:
+%       - coeff: array with the results. First element is delay in bins;
+%                Second element is the multiplication factor used.
+%       - gof: (goodness of fit) is the error of the best match found
+%       - err: is the error matrix
+%       
+%
+%   Last modified: 18.04.2016 by Eugenio Senes
+
+
     index_start = find(x1>=tsignal,1,'first');
     y1 = y1(index_start:end);
     y2 = y2(index_start:end);
@@ -15,7 +33,7 @@ function [coeff,gof,err] = correlationMethod( x1,y1,x2,y2,wind,tsignal)
 %     x2 = 1e9*x2(wind);
 %     y2 = abs(y2(wind));    
 
-    x_offset = linspace(-100,130,500)';
+    x_offset = linspace(-150,10,500)';
     y_multipl = linspace(0.1,1.9,100);
     err = zeros(length(x_offset),length(y_multipl));
 
@@ -25,8 +43,7 @@ function [coeff,gof,err] = correlationMethod( x1,y1,x2,y2,wind,tsignal)
         y2_temp = interp1(x2_temp,y2,x1);
         noNaN_indexes = find(~isnan(y2_temp));    
 
-
-        y2_temp_comp = y2_temp(noNaN_indexes)*y_multipl;
+        y2_temp_comp = y2_temp(noNaN_indexes)'*y_multipl;
         y1_temp = y1(noNaN_indexes);
 
         err(j,:) = sum((y2_temp_comp-repmat(y1_temp,1,size(y2_temp_comp,2))).^2);

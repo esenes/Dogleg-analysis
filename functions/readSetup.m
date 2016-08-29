e21d5764c162d54039de3e1c7c2c2ece5acc8e90
Eugenio Senes <esenes@users.noreply.github.com>
2016-08-29 21:11:05 +0200

    Add files via upload

--- functions/readSetup.m
+++ functions/readSetup.m
@@ -1,4 +1,4 @@
-function [ pathR, pathT, pathE ] = readSetup()
+function [ pathR, pathT, pathE, pathP, pathF ] = readSetup()
 %	readSetup.m reads the setup.dogleg file and returns the paths for
 %	reading, temp and exp_files
 %   
@@ -13,11 +13,13 @@ if fileID == -1
     error('Generate a setup file first ! (Please run setup.m before this script)')
 end
 
-fullTxt = textscan(fileID,'%s %s %s %s %s %s ');
+fullTxt = textscan(fileID,'%s %s %s %s %s %s %s %s %s %s');
 
 pathR = fullTxt{2}{1};
 pathT = fullTxt{2}{2};
 pathE = fullTxt{2}{3};
+pathP = fullTxt{2}{4};
+pathF = fullTxt{2}{5};
 
 fclose(fileID);
 

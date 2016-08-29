6a355b035de923d7206e096c1b4e1bb9d17dfbf4
Eugenio Senes <esenes@users.noreply.github.com>
2016-08-29 20:58:57 +0200

    Add files via upload

--- functions/readSetup.m
+++ functions/readSetup.m
@@ -1,22 +1,18 @@
 function [ pathR, pathT, pathE ] = readSetup()
-%	bpmcal.m take a list containing the bpm readings and calibrate it.
-%   Calibration parameters are hardcoded and were provided by J.G.Navarro 
-%   on 05.04.2016 via email
+%	readSetup.m reads the setup.dogleg file and returns the paths for
+%	reading, temp and exp_files
 %   
-%   Inputs:
-%     - data: list containing the bpm signal
-%     - BPM_name: string containing the name of the BPM. Not case
-%       insensitive
-%     
-%   Outputs:
-%     - cal_data
-%     
-%   Last modified: 05.04.2016 by Eugenio Senes
+%   Last modified: 19.08.2016 by Eugenio Senes
+
 [dirpath,~,~]=fileparts(mfilename('fullpath'));
 mainDir = dirpath(1:end-10);
 setupPath = [mainDir filesep 'setup.dogleg'];
 
 fileID = fopen(setupPath, 'r');
+if fileID == -1
+    error('Generate a setup file first ! (Please run setup.m before this script)')
+end
+
 fullTxt = textscan(fileID,'%s %s %s %s %s %s ');
 
 pathR = fullTxt{2}{1};
